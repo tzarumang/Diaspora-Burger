00ebbe54dcb39a0e340eb82cba82e74eadd22e7e
Tzar Umang <tzar@hiveonthedot.com>
2012-09-28 11:20:04 +0800

    Update app/models/request.rb

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,3 @@
-#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   t
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-
 class Request
   include Diaspora::Federated::Base
   include ActiveModel::Validations

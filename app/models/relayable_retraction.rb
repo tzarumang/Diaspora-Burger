5b7284dcda1236e2d4bf6592050b6a3b22fb6f3c
Tzar Umang <tzar@hiveonthedot.com>
2012-09-28 11:19:14 +0800

    Update app/models/relayable_retraction.rb

--- app/models/relayable_retraction.rb
+++ app/models/relayable_retraction.rb
@@ -1,7 +1,3 @@
-#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-
 class RelayableRetraction < SignedRetraction
   xml_name :relayable_retraction
   xml_attr :parent_author_signature

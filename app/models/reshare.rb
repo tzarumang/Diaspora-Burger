6d2439546798eb1cfa4da105b84951296bb24e82
Tzar Umang <tzar@hiveonthedot.com>
2012-09-28 11:22:38 +0800

    Update app/models/reshare.rb

--- app/models/reshare.rb
+++ app/models/reshare.rb
@@ -1,7 +1,3 @@
-#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-
 class Reshare < Post
 
   belongs_to :root, :class_name => 'Post', :foreign_key => :root_guid, :primary_key => :guid
